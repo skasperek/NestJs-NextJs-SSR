608b04c272e3ec41fa458b38a546eda78f1a8076
Sebastian Kasperek <kasperekseba@gmail.com>
2022-05-01 13:44:29 +0200

    Added nextjs build output to gitignore

--- be/Web/src/main.ts
+++ be/Web/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './DependencyInjection/app.module';
 import { RenderModule } from 'nest-next';
 import Next from 'next';
+import * as path from 'path';
 
 const bootstrap = async () => {
   const app = Next({
     dev: process.env.NODE_ENV !== 'production',
+    dir: "./src"
   })
 
   await app.prepare();
